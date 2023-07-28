a7b3e6b5ab840d0739fa962734c0d64383f73e75
solee0022 <solee0022@postech.ac.kr>
2023-07-28 16:31:54 +0900

    update monotonic_align

---
+++ monotonic_align/test_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# sbatch
+#SBATCH -J setup # job name
+#SBATCH -o %x_%j.out
+#SBATCH -p 2080ti #queue name or patition name
+#SBATCH -t 72:00:00 # Run time
+
+#SBATCH   --gres=gpu:1
+#SBTACH   --ntasks=1
+#SBATCH   --tasks-per-node=1
+#SBATCH   --cpus-per-task=1
+
+cd  $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+
+echo "CUDA_HOME=$CUDA_HOME"
+
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+
+
+srun -l /bin/hostname
+
+srun -l /bin/pwd
+
+srun -l /bin/date
+
+
+echo "START"
+
+python /home/solee0022/tts-asr/XPhoneBERT/VITS_with_XPhoneBERT/monotonic_align/setup.py build_ext --inplace
+
+date
+echo "END"
